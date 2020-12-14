566e087e885b1a0bff800d26773a018d292864fe
David Blocher <blocher.10@gmail.com>
2020-12-14 12:34:50 -0500

    Initial commit

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
 require('dotenv').config();
+//add AWS service access
+var AWS = require("aws-sdk");
+AWS.config.update({region: 'us-east-1'});
+var lexruntime = new AWS.LexRuntime();
+
+
+AWS.config.getCredentials(function(err) {
+  if (err) console.log(err.stack);
+  // credentials not loaded
+  else {
+    console.log("Access key:", AWS.config.credentials.accessKeyId);
+  }
+});
+
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 const TOKEN = process.env.TOKEN;
@@ -10,16 +24,22 @@ bot.on('ready', () => {
 });
 
 bot.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('pong');
-    msg.channel.send('pong');
-
-  } else if (msg.content.startsWith('!kick')) {
-    if (msg.mentions.users.size) {
-      const taggedUser = msg.mentions.users.first();
-      msg.channel.send(`You wanted to kick: ${taggedUser.username}`);
-    } else {
-      msg.reply('Please tag a valid user!');
-    }
+  if (msg.author.id != '788062078140153868') {
+    var params = {
+        botAlias: '$LATEST', /* required, has to be '$LATEST' */
+        botName: 'RoboVapeDawg', /* required, the name of you bot */
+        inputText: msg.content, /* required, your text */
+        userId: 'DiscordMessage', /* required, arbitrary identifier */
+      };
+      lexruntime.postText(params, function(err, data) {
+        if (err) console.log(err, err.stack); // an error occurred
+        else {
+          if ((data.message =='Sorry, can you please repeat that?') || (data.message =='Sorry, I could not understand. Goodbye.')) {
+            console.log(data.message);
+          } else {
+            msg.channel.send(data.message);   // successful response
+          };
+        };         
+      });
   }
 });
